7e15ddecf6dd31c1ff2ff72d32f39b9cfb6538c3
j8xixo12 <as2266317@gmail.com>
2021-06-05 13:18:57 +0800

    feat: completed the option class and test case

--- gtests/main.cpp
+++ gtests/main.cpp
@@ -13,6 +13,19 @@ TEST(OptionTest, Constructor)
   EXPECT_EQ(test_obj->get_underlying_asset(), 100.0);
 }
 
+TEST(OptionTest, CopyConstructor)
+{
+  option_type *test_obj = new option_type(100.0, 0.2, 0.01, 10, 100.0);
+  option_type copy_obj(*test_obj);
+
+  EXPECT_NE(&copy_obj, test_obj);
+
+  EXPECT_EQ(test_obj->get_strike_price(), copy_obj.get_strike_price());
+  EXPECT_EQ(test_obj->get_volatility(), copy_obj.get_volatility());
+  EXPECT_EQ(test_obj->get_interest_rate(), copy_obj.get_interest_rate());
+  EXPECT_EQ(test_obj->get_expire_date(), copy_obj.get_expire_date());
+  EXPECT_EQ(test_obj->get_underlying_asset(), copy_obj.get_underlying_asset());
+}
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
--- include/option.hpp
+++ include/option.hpp
@@ -6,9 +6,47 @@ class Option {
 public:
   Option() = default;
   explicit Option(double strike_price, double volatility, double interest_rate,
-                  date expire_date, asset underlying_asset) :
+                  date expire_date, asset underlying_asset):
             strike_price_(strike_price), volatility_(volatility), interest_rate_(interest_rate),
             expire_date_(expire_date), underlying_asset_(underlying_asset) {}
+
+  Option(const Option &other) : strike_price_(other.strike_price_), volatility_(other.volatility_),
+            interest_rate_(other.interest_rate_), expire_date_(other.expire_date_),
+            underlying_asset_(other.underlying_asset_) {}
+
+  Option(Option &&other) noexcept : strike_price_(other.strike_price_), volatility_(other.volatility_),
+            interest_rate_(other.interest_rate_), expire_date_(std::move(other.expire_date_)),
+            underlying_asset_(std::move(other.underlying_asset_)) {}
+
+  ~Option() = default;
+
+  Option& operator=(const Option &other) {
+    if (this == &other) return *this;
+    this->strike_price_ = other.strike_price_;
+    this->volatility_ = other.volatility_;
+    this->interest_rate_ = other.interest_rate_;
+    // TODO: Careful if the implementation of date is change,
+    // that the way of copy expire_date_ should be rewrite.
+    this->expire_date_ = other.expire_date_;
+    // TODO: ditto
+    this->underlying_asset_ = other.underlying_asset_;
+
+    return *this;
+  }
+
+  Option& operator=(Option &&other) noexcept {
+    if (this == &other) return *this;
+    this->strike_price_ = other.strike_price_;
+    this->volatility_ = other.volatility_;
+    this->interest_rate_ = other.interest_rate_;
+    // TODO: Careful if the implementation of date is change,
+    // that the way of copy expire_date_ should be rewrite.
+    this->expire_date_ = std::move(other.expire_date_);
+    // TODO: ditto
+    this->underlying_asset_ = std::move(other.underlying_asset_);
+
+    return *this;
+  }
 
   double get_strike_price(void) { return this->strike_price_; }
   double get_volatility(void) { return this->volatility_; }
